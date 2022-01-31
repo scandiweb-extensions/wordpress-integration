64fde36c6a08b3661749d83234339e6d68969ffa
Raivis Dejus <raivisd@scandiweb.com>
2022-01-31 15:47:51 +0200

    Re-adding files to prevent instance update error

---
+++ app/code/ScandiPWA/WordPressIntegration/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Scandiweb_WordPressIntegration',
+    __DIR__
+);
